c272c465e907b508ef9687d913740fac00b7381b
Wael Asmar <wasmar@c1r2s5.42beirut.com>
2025-04-02 08:20:42 +0300

    norm

--- error_handling.c
+++ error_handling.c
@@ -6,65 +6,77 @@
 /*   By: wasmar <wasmar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 09:48:15 by schaaban          #+#    #+#             */
-/*   Updated: 2025/04/02 07:26:45 by wasmar           ###   ########.fr       */
+/*   Updated: 2025/04/02 08:18:20 by wasmar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-int valid_identifier_helper(t_token *head)
+int	valid_identifier_helper(t_token *head)
 {
-    int count;
-    count = 0;
-    int i = 0;
-    if(head -> token[0] && !((head -> token[0] >= 'a' && head -> token[0] <= 'z') || (head -> token[0] >= 'A' && head -> token[0] <= 'Z') || (head -> token[0] == '_')))
-    {
-        ft_putendl_fd_two(head->token,": not a valid identifier",2);
-        count ++;
-    }
-    i = 1;
-    while(head -> token[i] && head -> token[i] != '=')
-    {
-        if(!((head -> token[i] >= 'a' && head -> token[i] <= 'z') || (head -> token[i] >= 'A' && head -> token[i] <= 'Z') || (head -> token[i] == '_') || (head -> token[i] >= '0' && head -> token[i] <='9')))
-        {
-            ft_putendl_fd_two(head->token,": not a valid identifier",2);
-            count ++;
-        }
-        i ++;
-    }
-    return (count);
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	if (head->token[0] && !((head->token[0] >= 'a' && head->token[0] <= 'z')
+			|| (head->token[0] >= 'A' && head->token[0] <= 'Z')
+			|| (head->token[0] == '_')))
+	{
+		ft_putendl_fd_two(head->token, ": not a valid identifier", 2);
+		count++;
+	}
+	i = 1;
+	while (head->token[i] && head->token[i] != '=')
+	{
+		if (!((head->token[i] >= 'a' && head->token[i] <= 'z')
+				|| (head->token[i] >= 'A' && head->token[i] <= 'Z')
+				|| (head->token[i] == '_') || (head->token[i] >= '0'
+					&& head->token[i] <= '9')))
+		{
+			ft_putendl_fd_two(head->token, ": not a valid identifier", 2);
+			count++;
+		}
+		i++;
+	}
+	return (count);
 }
-int valid_identifier(t_token *head)
+int	valid_identifier(t_token *head)
 {
-    int i = 1;
-        head = head -> next;
-    int count = 0;
-    while(head && head -> type ==  WORD)
-    {
-        count = valid_identifier_helper(head);
-        head = head -> next;
-    }
-    if(count > 0)
-        return (0);
-    else
-        return (1);
+	int	i;
+	int	count;
+
+	i = 1;
+	head = head->next;
+	count = 0;
+	while (head && head->type == WORD)
+	{
+		count = valid_identifier_helper(head);
+		head = head->next;
+	}
+	if (count > 0)
+		return (0);
+	else
+		return (1);
 }
 
-int invalid_option(t_token *head)
+int	invalid_option(t_token *head)
 {
-    int count = 0;
-    head = head -> next;
-    while(head)
-    {
-        if(head -> token[0] == '-')
-        {
-            printf("unset : -%c: invalid option\n",head -> token[1]);
-            count ++;
-        }
-        head = head -> next;
-    }
-    if(count > 0)
-        return (0);
-    else 
-        return (1);
+	int	count;
+
+	count = 0;
+	head = head->next;
+	while (head)
+	{
+		if (head->token[0] == '-')
+		{
+			printf("unset : -%c: invalid option\n", head->token[1]);
+			count++;
+		}
+		head = head->next;
+	}
+	if (count > 0)
+		return (0);
+	else
+		return (1);
 }
--- heredoc_expand.c
+++ heredoc_expand.c
@@ -3,209 +3,233 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc_expand.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hackme <hackme@student.42.fr>              +#+  +:+       +#+        */
+/*   By: wasmar <wasmar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 12:23:12 by schaaban          #+#    #+#             */
-/*   Updated: 2025/04/01 09:05:54 by hackme           ###   ########.fr       */
+/*   Updated: 2025/04/02 08:18:58 by wasmar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
- #include "header.h"
+#include "header.h"
 
-void expand_and_replace_heredoc(char **mainstr,char *str, int end,t_shell *shell)
+void	expand_and_replace_heredoc(char **mainstr, char *str, int end,
+		t_shell *shell)
 {
-    if(!str[0])
-    {
-            (*mainstr) = "";
-            return;
-    }
-    char *s;
-    int i = 0;
-    int x = 0;
-    s = ft_strdup((*mainstr) + end,shell->mallo);
-    int len = strlen(str) + strlen(s);
-    (*mainstr) = ft_malloc(shell->mallo,len + 1);
-    while(str[i])
-    {
-        (*mainstr)[x] = str[i];
-        i ++;
-        x ++;
-    }
-    i = 0;
-    while(s[i])
-    {
-        (*mainstr)[x] = s[i];
-        i ++;
-        x ++;
-    }
-    (*mainstr)[x] = '\0';
+	char	*s;
+	int		i;
+	int		x;
+	int		len;
 
-}
-char *create_array_till_dollar_h(char *input,int index,t_shell *shell)
-{
-    char * new_string;
-    int i;
-    i = 0;
-    new_string = NULL;
-    new_string = ft_malloc(shell->mallo,index+1);
-    while(i < index)
-    {
-        new_string[i] = input[i];
-        i++;
-    }
-    new_string[i] = '\0';
-    return(new_string);
-}
-char *check_char_after_dollar_and_expand1(char *str,t_env *envp,t_shell *exitcode)
-{
-    char *new_string = NULL;
-    if((str[0] == '$' && str[1] == '?'))
-    {
-     new_string = ft_itoa(exitcode -> exit_code,exitcode->mallo);
-    }
-     else if(str[0] == '$' && (str[1] == '"' || str[1] == '\'') )
-    {
-        new_string = ft_strdup(str+1,exitcode->mallo);
-    }
-    else if(str[0] == '$' && (str[1] >= '0' && str[1] <= '9'))
-    {
-        new_string = ft_strdup(str+2,exitcode->mallo);
-    }
-    else if((is_alphanumeric(str[1]) == 0 && (str[1] != '_')))
-    {
-        new_string = ft_strdup(str,exitcode->mallo);
-    }
-    else if((is_alphanumeric(str[1]) == 1 || (str[1] == '_')))
-     {//check ffirst charr
-        new_string = expand_dollar(str,envp,exitcode);
-     }
-    //  printf("null1");
-    return (new_string);
-}
-int check_if_quotes_exit(char *input)
-{
-    int count;
-    int i;
-    i = 0;
-    count = 0;
-    while(input[i])
-    {
-        if(input[i] == '"' || input[i] == '\'')
-            count ++;
-        i ++;
-    }
-    return (count);
-}
-void  process_dolloris_helper_h(int *i,char **input,char **str,t_env *env,t_shell *exitcode)
-{
-    int start = 0;
-    char *expanded;
-    int end;
-    char *to_expand;
-            if((*input)[(*i)] == '$')
-            {
-               (*str) = create_array_till_dollar_h((*input),(*i),exitcode);
-               start = (*i);
-               end = find_end_variable((*input),(*i));
-               to_expand = strndup((*input)+start,end -start);
-                expanded = check_char_after_dollar_and_expand1(to_expand,env,exitcode);
-                if(expanded == NULL)
-                {
-                    expand_and_replace_heredoc(input,(*str),end,exitcode);
-                    (*i) = strlen((*str)) -1;
-                }
-                else if(expanded)
-                {
-                    char *temp = ft_strjoin((*str),expanded,exitcode->mallo);
-                    (*str) = temp;
-                     expand_and_replace_heredoc(input,(*str),end,exitcode);
-                    (*i) = strlen((*str))-1 ;
-                    
-                }
-                else
-                    (*i)= end;
-            }
-}
-void process_dolloris_heredoc(char **input, t_env *env,t_shell *exitcode)
-{
-    int i = 0;
-    char *str = NULL;
-    int end = 0;
-    char *to_expand = NULL;
-    int start = 0;
-    char *expanded;
-    int len = 0;
-        while(input[i])
-        {
-            process_dolloris_helper_h(&i, input, &str, env,exitcode);
-            i++;
-        }
- }
-int find_end_of_quotes_h(char *str, char quote,int start)
-{
-	int x= start+1 ;
-	// printf("\n%s\n",str);
-	while(str[x])
+	if (!str[0])
 	{
-		if(str[x] == quote)
-			return(x);
+		(*mainstr) = "";
+		return ;
+	}
+	i = 0;
+	x = 0;
+	s = ft_strdup((*mainstr) + end, shell->mallo);
+	len = strlen(str) + strlen(s);
+	(*mainstr) = ft_malloc(shell->mallo, len + 1);
+	while (str[i])
+	{
+		(*mainstr)[x] = str[i];
+		i++;
 		x++;
 	}
-	return(-1);
-}
-void remove_quotes_and_replace_h(char **str,int start,t_shell *shell)
-{
-	int end = 0;
-	char *new = NULL;
-	int len =0;
-	end = find_end_of_quotes_h((*str),(*str)[start],start);
-	if(end == -1)
+	i = 0;
+	while (s[i])
 	{
-		return;
+		(*mainstr)[x] = s[i];
+		i++;
+		x++;
 	}
-	new = new_string((*str),start,end,shell);
-	len = strlen(new);
-	(*str) =ft_malloc(shell->mallo,len+1);
-	strcpy((*str),new);
-}  
-
-
-void remove_quotes_main_heredoc(char **str,t_shell *shell)
+	(*mainstr)[x] = '\0';
+}
+char	*create_array_till_dollar_h(char *input, int index, t_shell *shell)
 {
-    int i = 0, end = 0, single_quotes = 0, double_quotes = 0;
-    int len = 0;
-    char *copy;
+	char	*new_string;
+	int		i;
 
-    while ((*str)[i])
-    {
-        if ((*str)[i] == '"' && single_quotes == 0)
-        {
-            copy = ft_strdup((*str),shell->mallo);  // Make a copy before modifying
-            end = find_end_of_quotes_h(copy, '"', i);
+	i = 0;
+	new_string = NULL;
+	new_string = ft_malloc(shell->mallo, index + 1);
+	while (i < index)
+	{
+		new_string[i] = input[i];
+		i++;
+	}
+	new_string[i] = '\0';
+	return (new_string);
+}
+char	*check_char_after_dollar_and_expand1(char *str, t_env *envp,
+		t_shell *exitcode)
+{
+	char	*new_string;
 
-            if (end == -1 || end >= strlen(*str))
-                break;
+	new_string = NULL;
+	if ((str[0] == '$' && str[1] == '?'))
+	{
+		new_string = ft_itoa(exitcode->exit_code, exitcode->mallo);
+	}
+	else if (str[0] == '$' && (str[1] == '"' || str[1] == '\''))
+	{
+		new_string = ft_strdup(str + 1, exitcode->mallo);
+	}
+	else if (str[0] == '$' && (str[1] >= '0' && str[1] <= '9'))
+	{
+		new_string = ft_strdup(str + 2, exitcode->mallo);
+	}
+	else if ((is_alphanumeric(str[1]) == 0 && (str[1] != '_')))
+	{
+		new_string = ft_strdup(str, exitcode->mallo);
+	}
+	else if ((is_alphanumeric(str[1]) == 1 || (str[1] == '_')))
+	{ // check ffirst charr
+		new_string = expand_dollar(str, envp, exitcode);
+	}
+	//  printf("null1");
+	return (new_string);
+}
+int	check_if_quotes_exit(char *input)
+{
+	int	count;
+	int	i;
 
-            remove_quotes_and_replace_h(str, i,shell);
-            i = end - 1;
-            single_quotes = 0;
-            double_quotes = 0;
-            continue;
-        }
-        if ((*str)[i] == '\'' && double_quotes == 0)
-        {
-            copy = ft_strdup((*str),shell->mallo);
-            end = find_end_of_quotes_h(copy, '\'', i);
+	i = 0;
+	count = 0;
+	while (input[i])
+	{
+		if (input[i] == '"' || input[i] == '\'')
+			count++;
+		i++;
+	}
+	return (count);
+}
+void	process_dolloris_helper_h(int *i, char **input, char **str, t_env *env,
+		t_shell *exitcode)
+{
+	int		start;
+	char	*expanded;
+	int		end;
+	char	*to_expand;
+	char	*temp;
 
-            if (end == -1 || end >= strlen(*str))
-                break;
+	start = 0;
+	if ((*input)[(*i)] == '$')
+	{
+		(*str) = create_array_till_dollar_h((*input), (*i), exitcode);
+		start = (*i);
+		end = find_end_variable((*input), (*i));
+		to_expand = strndup((*input) + start, end - start);
+		expanded = check_char_after_dollar_and_expand1(to_expand, env,
+				exitcode);
+		if (expanded == NULL)
+		{
+			expand_and_replace_heredoc(input, (*str), end, exitcode);
+			(*i) = strlen((*str)) - 1;
+		}
+		else if (expanded)
+		{
+			temp = ft_strjoin((*str), expanded, exitcode->mallo);
+			(*str) = temp;
+			expand_and_replace_heredoc(input, (*str), end, exitcode);
+			(*i) = strlen((*str)) - 1;
+		}
+		else
+			(*i) = end;
+	}
+}
+void	process_dolloris_heredoc(char **input, t_env *env, t_shell *exitcode)
+{
+	int		i;
+	char	*str;
+	int		end;
+	char	*to_expand;
+	int		start;
+	char	*expanded;
+	int		len;
 
-            remove_quotes_and_replace_h(str, i,shell);
-            i = end - 1;
-            single_quotes = 0;
-            double_quotes = 0;
-            continue;
-        }
-        i++;
-    }
+	i = 0;
+	str = NULL;
+	end = 0;
+	to_expand = NULL;
+	start = 0;
+	len = 0;
+	while (input[i])
+	{
+		process_dolloris_helper_h(&i, input, &str, env, exitcode);
+		i++;
+	}
+}
+int	find_end_of_quotes_h(char *str, char quote, int start)
+{
+	int	x;
+
+	x = start + 1;
+	// printf("\n%s\n",str);
+	while (str[x])
+	{
+		if (str[x] == quote)
+			return (x);
+		x++;
+	}
+	return (-1);
+}
+void	remove_quotes_and_replace_h(char **str, int start, t_shell *shell)
+{
+	int		end;
+	char	*new;
+	int		len;
+
+	end = 0;
+	new = NULL;
+	len = 0;
+	end = find_end_of_quotes_h((*str), (*str)[start], start);
+	if (end == -1)
+	{
+		return ;
+	}
+	new = new_string((*str), start, end, shell);
+	len = strlen(new);
+	(*str) = ft_malloc(shell->mallo, len + 1);
+	strcpy((*str), new);
+}
+
+void	remove_quotes_main_heredoc(char **str, t_shell *shell)
+{
+	int		i = 0, end = 0, single_quotes = 0, double_quotes;
+	int		len;
+	char	*copy;
+
+	i = 0, end = 0, single_quotes = 0, double_quotes = 0;
+	len = 0;
+	while ((*str)[i])
+	{
+		if ((*str)[i] == '"' && single_quotes == 0)
+		{
+			copy = ft_strdup((*str), shell->mallo);
+				// Make a copy before modifying
+			end = find_end_of_quotes_h(copy, '"', i);
+			if (end == -1 || end >= strlen(*str))
+				break ;
+			remove_quotes_and_replace_h(str, i, shell);
+			i = end - 1;
+			single_quotes = 0;
+			double_quotes = 0;
+			continue ;
+		}
+		if ((*str)[i] == '\'' && double_quotes == 0)
+		{
+			copy = ft_strdup((*str), shell->mallo);
+			end = find_end_of_quotes_h(copy, '\'', i);
+			if (end == -1 || end >= strlen(*str))
+				break ;
+			remove_quotes_and_replace_h(str, i, shell);
+			i = end - 1;
+			single_quotes = 0;
+			double_quotes = 0;
+			continue ;
+		}
+		i++;
+	}
 }
